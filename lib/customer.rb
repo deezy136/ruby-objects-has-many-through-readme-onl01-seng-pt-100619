081f7442b3f23cb4314e802e0cc66a2be3269bae
deezy13 <deezy13@gmail.com>
2019-11-19 16:36:33 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,9 +2,9 @@ class Customer
   attr_accessor :name, :age 
   
   def initialize(name, age)
-    
-    @name = name 
+        @name = name 
     @age = age 
+    @@all << self 
   end 
   
   def self.all 
